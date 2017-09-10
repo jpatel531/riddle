5649968a65ea1171069637e668a3339ebfbbaa79
Jamie Patel <jamie@notespublication.com>
2017-09-10 16:05:58 +0200

    moar

--- app/editor.jsx
+++ app/editor.jsx
@@ -2,6 +2,10 @@
 import {h, Component, render} from 'preact'
 import Quill from 'quill'
 
+function getWordCount(text) {
+  return text.length > 0 ? text.split(/\s+/).length : 0;
+}
+
 export default class Editor extends Component {
 
 	componentDidMount() {
@@ -27,32 +31,59 @@ export default class Editor extends Component {
 
     let lineHeight = window.getComputedStyle(this.editor.root, null).getPropertyValue('line-height')
     let editorHeight = (parseInt(lineHeight, 10) * this.props.visibleLineCount) + 'px'
-    this.editor.root.style.height = editorHeight
+    this.editor.root.style.maxHeight = editorHeight
 
-    this.editor.focus()
     this.editor.on('text-change', (delta, oldDelta, source) => {
-    	if (source == "user") {
-    		let lines = this.editor.getLines().reverse()
-    		let opacity = 1
-        let grade = opacity / this.props.visibleLineCount
-    		for (let line of lines) {
-    			line.domNode.style.opacity = opacity
-    			opacity -= grade
-    		}
-    	}
+  		let lines = this.editor.getLines().reverse()
+  		let opacity = 1
+      let grade = opacity / this.props.visibleLineCount
+  		for (let line of lines) {
+        // console.log(line)
+  			line.domNode.style.opacity = opacity
+  			opacity -= grade
+  		}
+
+      let deletedHTML, deletedWordCount;
+      if (lines.length > 5) {
+        // let line = this.editor.getLine(0);
+        // console.log(line)
+        // line.remove()
+        let firstLine = lines.reverse()[0];
+        console.log(firstLine)
+        deletedHTML = firstLine.domNode.outerHTML
+        deletedWordCount = getWordCount(firstLine.domNode.innerText.trim())
+        firstLine.remove()
+      }
+
       let text = this.editor.getText().trim()
-      let wordCount = text.length > 0 ? text.split(/\s+/).length : 0;
+      let wordCount = getWordCount(text);
       this.props.updateWordCount(wordCount)
+      this.props.onContentChanged(
+        this.editor.root.innerHTML, // visible html
+        getWordCount(this.editor.getText().trim()), // visible word count
+        deletedHTML,
+        deletedWordCount
+      )
     })
+
+    this.editor.clipboard.dangerouslyPasteHTML(0, this.props.content)
+    this.editor.focus()
 	}
 
   shouldComponentUpdate(nextProps, nextState) {
     return false
   }
 
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.content != this.editor.root.innerHTML) {
+      this.editor.clipboard.dangerouslyPasteHTML(0, this.props.content)
+    }
+  }
+
 	render() {
 		return (
-			<div>
+			<div id="editor-wrapper">
 				<div id="toolbar"></div>
 				<div id="editor"></div>
 			</div>
--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,71 @@
 import {h, Component, render} from 'preact'
 import { Header, Title, Footer, Button, ButtonGroup, NavGroup } from 'preact-photon'
 import Editor from './editor.jsx'
+import { remote } from 'electron'
+import fs from 'mz/fs'
+const { dialog } = remote
+import path from 'path'
+
+// Wrap inconvenient APIs with promises
+function showSaveDialogAsync (options) {
+  return new Promise((resolve, reject) => {
+    try {
+      const files = dialog.showSaveDialog(options || {
+      	filters: {
+      		name: 'Riddles', extensions: ['riddle']
+      	}
+      })
+      resolve(files)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+function saveFileAsync (fileName, visibleHTML, visibleWordCount, deletedHTML, deletedWordCount, targetWords, lineVisibility) {
+  if (typeof fileName !== 'string') throw Error('Filename must be a string')
+  return new Promise((resolve, reject) => {
+  	let encoded = JSON.stringify({
+  		visibleHTML,
+  		visibleWordCount,
+  		deletedHTML,
+  		deletedWordCount,
+  		targetWords,
+  		lineVisibility
+  	})
+  	if (!fileName.endsWith(".riddle")) {
+  		fileName += ".riddle"
+  	}
+
+  	fs.writeFile(fileName, encoded, (err) => {
+  		if (err) {
+  			reject(err)
+  		} else {
+  			resolve(fileName)
+  		}
+  	})
+  })
+}
+
+function showOpenDialogAsync (options) {
+  const htmlDefaults = {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Riddles',
+        extensions: ['riddle'],
+      },
+    ],
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      const files = dialog.showOpenDialog(options || htmlDefaults)
+      resolve(files)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
 
 class App extends Component {
 
@@ -11,7 +76,11 @@ class App extends Component {
 			onboarding: true,
 			targetWords: null,
 			lineCount: null,
-			wordCount: 0
+			filename: null,
+			visibleHTML: "",
+			deletedHTML: "",
+			visibleWordCount: 0,
+			deletedWordCount: 0
 		}
 	}
 
@@ -30,15 +99,88 @@ class App extends Component {
 		this.setState({onboarding: this.state.targetWords == null})
 	}
 
+	handleClickSave() {
+		if (this.state.onboarding) {
+			return;
+		}
+
+    let filename = this.state.filename
+    if (filename) {
+    	saveFileAsync(
+    		filename,
+    		this.state.visibleHTML,
+    		this.state.visibleWordCount,
+    		this.state.deletedHTML,
+    		this.state.deletedWordCount,
+    		this.state.targetWords,
+    		this.state.lineCount,
+    	).catch((err) => console.error(err))
+      return
+    }
+
+    showSaveDialogAsync()
+      .then((filename) => {
+	    	return saveFileAsync(
+	    		filename,
+	    		this.state.visibleHTML,
+	    		this.state.visibleWordCount,
+	    		this.state.deletedHTML,
+	    		this.state.deletedWordCount,
+	    		this.state.targetWords,
+	    		this.state.lineCount,
+	    	)
+      })
+      .then((filename) => this.setState({filename}))
+      .catch((err) => console.error(err))
+	}
+
+	handleClickOpen() {
+    showOpenDialogAsync()
+      .then((filenames) => {
+      	let filename = filenames[0];
+      	if (filename === this.state.filename) {
+      		return
+      	}
+        this.setState({ filename  })
+        return fs.readFile(filenames[0], 'utf8')
+      })
+      .then((content) => this.handleOpeningContent(content))
+      .catch(console.error)
+	}
+
+	handleOpeningContent(content) {
+		let decoded = JSON.parse(content)
+		this.setState({
+			onboarding: false,
+			lineCount: decoded.lineVisibility,
+			targetWords: decoded.targetWords,
+			deletedHTML: decoded.deletedHTML,
+			deletedWordCount: decoded.deletedWordCount,
+			visibleHTML: decoded.visibleHTML,
+			visibleWordCount: decoded.visibleWordCount
+		})
+	}
+
 	updateWordCount(wordCount) {
 		this.setState({wordCount})
+	}
+
+	onContentChanged(visibleHTML, visibleWordCount, deletedHTML = "", deletedWordCount = 0) {
+		this.setState({
+			visibleHTML,
+			visibleWordCount,
+			deletedHTML: this.state.deletedHTML + deletedHTML,
+			deletedWordCount: this.state.deletedWordCount + deletedWordCount
+		})
 	}
 
 	render() {
 		let view;
 		if (!this.state.onboarding) {
-			console.log(this.state)
 			view = <Editor
+				ref={editor => this.editor = editor}
+				content={this.state.visibleHTML}
+				onContentChanged={this.onContentChanged.bind(this)}
 				targetWords={this.state.targetWords}
 				updateWordCount={this.updateWordCount.bind(this)}
 				visibleLineCount={this.state.lineCount} />
@@ -56,14 +198,35 @@ class App extends Component {
 		return (
 			<div className="window">
 				<Header>
-					<Title>Riddle</Title>
+					<Title>{this.state.filename ? path.basename(this.state.filename) : 'Riddle'}</Title>
+ 					<div className='toolbar-actions'>
+            <ButtonGroup>
+              <Button
+                mini
+                icon='floppy'
+                enabled={false}
+                onClick={this.handleClickSave.bind(this)}
+              />
+              <Button
+                mini
+                icon='folder'
+                onClick={this.handleClickOpen.bind(this)}
+              />
+            </ButtonGroup>
+            <Button
+              mini
+              icon='plus'
+              class='pull-right'
+              onClick={this.handleClickNew}
+            />
+          </div>
 				</Header>
 				<div className="window-content">
 					{view}
 				</div>
         <Footer>
           <Title>
-            {this.state.wordCount}/{this.state.targetWords} Words
+            {this.state.visibleWordCount + this.state.deletedWordCount}/{this.state.targetWords} Words
           </Title>
         </Footer>
 			</div>
